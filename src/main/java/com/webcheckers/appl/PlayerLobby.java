f753a114075537a4e331a02ef1c0a17eaa0a1a9a
Alan Read <adr4069@g.rit.edu>
2019-09-28 16:24:43 -0400

    Added a hashset to list signed in players

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -1,8 +1,16 @@
 package com.webcheckers.appl;
+import com.webcheckers.model.Player;
+import java.util.HashSet;
 
 public class PlayerLobby {
 
-    public PlayerLobby(){
+    HashSet<Player> players;
 
+    public PlayerLobby(){
+        this.players = new HashSet<>();
+    }
+
+    public void addPlayer(Player player){
+        players.add(player);
     }
 }
