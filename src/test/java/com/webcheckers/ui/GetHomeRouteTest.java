006f04271df7f76e502f8c20f5926cd5eb815ea5
Ryan Madulka <rtm1822@g.rit.edu>
2019-10-19 23:07:19 -0400

    created Test Packages

---
+++ src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
@@ -0,0 +1,48 @@
+package com.webcheckers.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.webcheckers.appl.PlayerLobby;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import spark.HaltException;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Session;
+import spark.TemplateEngine;
+
+public class GetHomeRouteTest {
+
+    private GetHomeRoute CuT;
+
+    private PlayerLobby playerLobby;
+
+    // mock objects
+    private Request request;
+    private Session session;
+    private TemplateEngine engine;
+    private Response response;
+
+    /**
+     * Setup new mock objects for each test.
+     */
+    @BeforeEach
+    public void setup() {
+        request = mock(Request.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        response = mock(Response.class);
+        engine = mock(TemplateEngine.class);
+
+        playerLobby = new PlayerLobby();
+        CuT = new GetHomeRoute(engine, playerLobby);
+    }
+}
