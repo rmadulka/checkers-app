fc09e8cdd77f9f2c2ccc8e2ed24018ee436d587c
spencer josi <saj4750@rit.edu>
2019-10-23 23:10:25 -0400

    completed PlayerTest again x2

--- src/test/java/com/webcheckers/model/PlayerTest.java
+++ src/test/java/com/webcheckers/model/PlayerTest.java
@@ -3,6 +3,8 @@ package com.webcheckers.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -12,28 +14,49 @@ public class PlayerTest {
     public static final String name = "Joe Mama";
     public static final Player samplePlayer = new Player(name);
 
-
     @BeforeEach
-    public void setup(){
+    public void setup() {
         CuT = new Player(name);
     }
 
     @Test
-    public void test_get_name(){
+    public void test_get_name() {
         assertNotNull(CuT.getName());
         assertEquals(samplePlayer.getName(), CuT.getName());
-
     }
+
     @Test
-    public void test_get_in_game(){
+    public void test_get_in_game() {
         assertEquals(samplePlayer.getInGame(), CuT.getInGame());
     }
 
     @Test
-    public void test_set_in_game(){
+    public void test_set_in_game_different_statuses() {
         Player newPlayer = new Player("j");
         newPlayer.setInGame(true);
         assertNotEquals(newPlayer.getInGame(), CuT.getInGame());
     }
 
+    @Test
+    public void test_set_in_game_same_statuses() {
+        Player newPlayer = new Player("j");
+        newPlayer.setInGame(false);
+        assertNotEquals(newPlayer.getInGame(), CuT.getInGame());
+    }
+
+    @Test
+    public void test_equals_same(){
+        assertEquals(samplePlayer.equals(samplePlayer), CuT.equals(CuT));
+    }
+
+    @Test
+    public void test_equals_same_but_different_objects(){
+        assertEquals(samplePlayer.equals(CuT), CuT.equals(samplePlayer));
+    }
+
+    @Test
+    public void test_different_objects(){
+        ArrayList<String> a = new ArrayList<>();
+        assertEquals(a.equals(CuT), CuT.equals(a));
+    }
 }
