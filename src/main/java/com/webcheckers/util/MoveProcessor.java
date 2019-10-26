9e0fac9b5d494d8c087836754058d1a1918870b3
Ryan Madulka <rtm1822@g.rit.edu>
2019-10-26 16:09:40 -0400

    Began submit turn funcitonality

--- src/main/java/com/webcheckers/util/MoveProcessor.java
+++ src/main/java/com/webcheckers/util/MoveProcessor.java
@@ -2,14 +2,23 @@ package com.webcheckers.util;
 
 import com.webcheckers.model.Board;
 import com.webcheckers.model.Move;
+import com.webcheckers.model.Player;
 import com.webcheckers.model.Space;
 
-public class MoveValidation {
+import java.util.Stack;
+
+public class MoveProcessor {
 
     public static boolean validateMove (Move move, Board board) {
         Space[][] gameBoard = board.getBoard();
         return gameBoard[move.getEndRow()][move.getEndCell()].isValid() &&
                 move.getStartRow() + 1 == move.getEndRow() &&
                 (move.getStartCell() + 1 == move.getEndCell() || move.getStartCell() - 1 == move.getEndCell());
+    }
+
+    public static boolean processMoves(Player player, Board board){
+        Stack<Move> turnStack = player.getTurnStack();
+        //TODO assert proper players turn
+        return false;
     }
 }
