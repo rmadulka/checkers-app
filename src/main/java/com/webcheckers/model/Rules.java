8bd9a53b1fff17a61e59cabf41262db8abfb35ea
Alan Read <adr4069@g.rit.edu>
2019-10-31 14:35:22 -0400

    Fixed the design for move validation

---
+++ src/main/java/com/webcheckers/model/Rules.java
@@ -0,0 +1,60 @@
+package com.webcheckers.model;
+
+import java.util.Stack;
+
+public abstract class Rules {
+
+    public abstract boolean checkMoves (Move move, Board board);
+
+    /**
+     * During a turn, a player is required to perform a jump move if there is one. Furthermore, a player is not required
+     * to perform the jump move with the most jumps. A player can choose to do any jump move on the board, however, if
+     * the piece they choose to perform the jump move is able to do multiple jump moves, then the player is required to
+     * perform all the jump moves
+     *
+     * This method determines if there is an available jump move on the board
+     * @param board A board
+     * @return True if there is an available jump move
+     */
+    public boolean checkForJumpMove(Board board) {
+        Space[][] gameBoard = board.getBoard();
+        int negOne = 1;
+        if(board.getActiveColor() == Piece.pieceColor.WHITE) {
+            negOne = -1;
+        }
+        for (int row = 0; row < gameBoard.length; row ++) {
+            for(int col = 0; col < gameBoard.length; col ++) {
+                if (!(row + negOne * 2 > gameBoard.length - 1 || row + negOne * 2 < 0)) {
+                    //check that this is the moving player's piece
+                    if (gameBoard[row][col].getPiece() != null &&
+                            gameBoard[row][col].getPiece().getColor() == board.getActiveColor()) {
+                        //check out of bounds, adjacent diagonal right piece is opponent and there is an empty space after
+                        //TODO Fix law of demeter here
+                        if (!(col + 2 > gameBoard.length - 1) && gameBoard[row + negOne][col + 1].getPiece() != null &&
+                                gameBoard[row + negOne][col + 1].getPiece().getColor() != board.getActiveColor() &&
+                                gameBoard[row + negOne * 2][col + 2].getPiece() == null) {
+                            return true;
+                        }
+                        //check out of bounds, adjacent diagonal left piece is opponent and there is an empty space after
+                        //TODO Fix law of demeter here
+                        if (col - 2 >= 0 && gameBoard[row + negOne][col - 1].getPiece() != null &&
+                                gameBoard[row + negOne][col - 1].getPiece().getColor() != board.getActiveColor() &&
+                                gameBoard[row + negOne * 2][col - 2].getPiece() == null) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean processMoves(Player player, Board board){
+        Stack<Move> turnStack = player.getTurnStack();
+        while (!turnStack.empty()) {
+            board.makeMove(turnStack.remove(0));
+        }
+        //TODO assert proper players turn
+        return true;
+    }
+}
---
+++ src/main/java/com/webcheckers/model/SimpleMove.java
@@ -0,0 +1,27 @@
+package com.webcheckers.model;
+
+public class SimpleMove extends Rules{
+
+    /**
+     * Checks if a simple move is valid
+     *
+     * @param move  The move that the player made
+     * @param board The board
+     * @return True if a player made a valid move
+     */
+    public boolean validateSimpleMove(Move move, Board board) {
+        Space[][] gameBoard = board.getBoard();
+        int one = 1;
+        if (board.getActiveColor() == Piece.pieceColor.WHITE) {
+            one = -1;
+        }
+        return move.getStartRow() + one == move.getEndRow() &&
+                ((move.getStartCell() + 1 == move.getEndCell() || move.getStartCell() - 1 == move.getEndCell()) &&
+                        gameBoard[move.getStartRow()][move.getStartCell()].getPiece() != null);
+    }
+
+    @Override
+    public boolean checkMoves(Move move, Board board){
+        return validateSimpleMove(move, board);
+    }
+}
