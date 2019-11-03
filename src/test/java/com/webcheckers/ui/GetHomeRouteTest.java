160f70d2641648f6f9dd416d0e93be6b1059be5d
spencer josi <saj4750@rit.edu>
2019-11-02 20:13:33 -0400

    added documentation in test classes

--- src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetHomeRouteTest.java
@@ -2,7 +2,6 @@ package com.webcheckers.ui;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -11,17 +10,13 @@ import com.webcheckers.appl.PlayerLobby;
 import com.webcheckers.model.Player;
 import com.webcheckers.util.Message;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import spark.HaltException;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.Session;
 import spark.TemplateEngine;
-
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class GetHomeRouteTest {
@@ -57,24 +52,14 @@ public class GetHomeRouteTest {
      */
     @Test
     public void new_session() {
-        // To analyze what the Route created in the View-Model map you need
-        // to be able to extract the argument to the TemplateEngine.render method.
-        // Mock up the 'render' method by supplying a Mockito 'Answer' object
-        // that captures the ModelAndView data passed to the template engine
+
         final TemplateEngineTester testHelper = new TemplateEngineTester();
         when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
-
-        // Invoke the test
         CuT.handle(request, response);
 
-        // Analyze the results:
-        //   * model is a non-null Map
         testHelper.assertViewModelExists();
         testHelper.assertViewModelIsaMap();
-        //   * model contains all necessary View-Model data
         testHelper.assertViewModelAttribute(GetHomeRoute.HOME_TITLE, GetHomeRoute.HOME_TITLE_TEXT);
-
-        //   * test view name
         testHelper.assertViewName(GetHomeRoute.VIEW_NAME);
     }
 
@@ -137,7 +122,6 @@ public class GetHomeRouteTest {
 
         CuT.handle(request, response);
 
-        //  *test that default message is the welcome message
         testHelper.assertViewModelAttribute(GetHomeRoute.MESSAGE, GetHomeRoute.WELCOME_MSG);
     }
 
@@ -170,11 +154,9 @@ public class GetHomeRouteTest {
             CuT.handle(request, response);
             fail("Redirects invoke halt exceptions.");
         } catch (HaltException e) {
-            // expected
+
         }
 
-        // Analyze the results:
-        //   * redirect to the Game view
         verify(response).redirect(WebServer.GAME_URL);
     }
 }
