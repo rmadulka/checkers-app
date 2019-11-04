3f40533b3a17b548ede1443a7e240f34f7ccd50e
Casper Loveless <col9088@rit.edu>
2019-11-04 01:31:37 -0500

    implemented some test cases for CheckOneJumpMoveTest

---
+++ src/test/java/com/webcheckers/model/CheckOneJumpMoveTest.java
@@ -0,0 +1,138 @@
+package com.webcheckers.model;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CheckOneJumpMoveTest {
+
+  /**
+   * Friendly object
+   */
+  private CheckOneJumpMove CuT;
+
+  /**
+   * Mock player object
+   */
+  private static Player redPlayer;
+
+  /**
+   * Mock player object
+   */
+  private static Player whitePlayer;
+
+  /**
+   * Mock board object
+   */
+  private static Board board;
+
+  /**
+   * Mock board grid
+   */
+  private static Space[][] spaces;
+
+  @BeforeEach
+  private void init() {
+    CuT = new CheckOneJumpMove();
+    redPlayer = new Player("Red");
+    whitePlayer = new Player("White");
+    board = new Board(redPlayer, whitePlayer);
+    spaces = board.getBoard();
+    for (int row = 0; row < 8; row++) {
+      for (int col = 0; col < 8; col++) {
+        if (row % 2 == 0 && col % 2 == 0) {
+          spaces[row][col] = new Space(col, true);
+        } else if (row % 2 == 1 && col % 2 == 1) {
+          spaces[row][col] = new Space(col, true);
+        } else {
+          new Space(col, false);
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testRedHasNoJumpMove() {
+    Position start = new Position(0, 0);
+    Space jumping = board.getSpace(new Position(1, 1));
+    jumping.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.WHITE));
+    Position end = new Position(2, 2);
+    Space ended = board.getSpace(end);
+    ended.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.RED));
+    Move move = new Move(start, end);
+    assertFalse(CuT.checkMoves(move, board));
+  }
+
+  @Test
+  public void testWhiteHasNoJumpMove() {
+    board.switchTurn();
+    Position start = new Position(5, 7);
+    Space jumping = board.getSpace(new Position(4, 6));
+    jumping.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.RED));
+    Position end = new Position(3, 5);
+    Space ended = board.getSpace(end);
+    ended.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.WHITE));
+    Move move = new Move(start, end);
+    assertFalse(CuT.checkMoves(move, board));
+  }
+
+  @Test
+  public void testRedHasOneJumpMoveRight() {
+    Position start = new Position(0, 0);
+    Space jumping = board.getSpace(new Position(1, 1));
+    jumping.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.WHITE));
+    Position end = new Position(2, 2);
+    Space ended = board.getSpace(end);
+    ended.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.RED));
+    Move move = new Move(start, end);
+    Space ahead = board.getSpace(new Position(3,3));
+    ahead.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.WHITE));
+    assertTrue(CuT.checkMoves(move, board));
+  }
+
+  @Test
+  public void testRedHasOneMoveJumpLeft() {
+    Position start = new Position(0, 0);
+    Space jumping = board.getSpace(new Position(1, 1));
+    jumping.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.WHITE));
+    Position end = new Position(2, 2);
+    Space ended = board.getSpace(end);
+    ended.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.RED));
+    Move move = new Move(start, end);
+    Space ahead = board.getSpace(new Position(3, 1));
+    ahead.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.WHITE));
+    assertTrue(CuT.checkMoves(move, board));
+  }
+
+  @Test
+  public void testWhiteHasOneJumpMoveRight() {
+    board.switchTurn();
+    Position start = new Position(7, 7);
+    Space jumping = board.getSpace(new Position(6, 6));
+    jumping.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.RED));
+    Position end = new Position(5, 5);
+    Space ended = board.getSpace(end);
+    ended.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.WHITE));
+    Move move = new Move(start, end);
+    Space ahead = board.getSpace(new Position(4,4));
+    ahead.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.RED));
+    assertTrue(CuT.checkMoves(move, board));
+  }
+
+  @Test
+  public void testWhiteHasOneMoveJumpLeft() {
+    board.switchTurn();
+    Position start = new Position(7, 7);
+    Space jumping = board.getSpace(new Position(6, 6));
+    jumping.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.RED));
+    Position end = new Position(5, 5);
+    Space ended = board.getSpace(end);
+    ended.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.WHITE));
+    Move move = new Move(start, end);
+    Space ahead = board.getSpace(new Position(4, 6));
+    ahead.place(new Piece(Piece.pieceType.SINGLE, Piece.pieceColor.RED));
+    assertTrue(CuT.checkMoves(move, board));
+  }
+}
