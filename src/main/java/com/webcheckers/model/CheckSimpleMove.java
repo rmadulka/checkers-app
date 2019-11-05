99d4db2a10a2bab34acb0e34cca0ad2cbee5a288
spencer josi <saj4750@rit.edu>
2019-11-04 22:19:47 -0500

    added documentation

--- src/main/java/com/webcheckers/model/CheckSimpleMove.java
+++ src/main/java/com/webcheckers/model/CheckSimpleMove.java
@@ -1,7 +1,15 @@
 package com.webcheckers.model;
 
+/**
+ * Checks if there are any available Simple moves in order to determine if the game is over
+ */
 public class CheckSimpleMove extends Rules {
 
+    /**
+     * iterates through the board ensuring that there is either at least a possible simple move or no simple moves
+     * @param board current board
+     * @return boolean whether there is a simple move
+     */
     private boolean checkForSimpleMove(Board board) {
         Space[][] gameBoard = board.getBoard();
         int negOne = 1;
@@ -50,7 +58,12 @@ public class CheckSimpleMove extends Rules {
         return false;
     }
 
-
+    /**
+     * calls checkForSimpleMove(), intended to be accessed being extended by Rules
+     * @param move The move the player made
+     * @param board The board
+     * @return boolean whether there is a simple move available
+     */
     @Override
     public boolean checkMoves(Move move, Board board) {
         return checkForSimpleMove(board);
