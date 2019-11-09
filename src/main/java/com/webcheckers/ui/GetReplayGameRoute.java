111fe61f99663dab1dfdb3e6d1cbcce546f79852
spencer josi <saj4750@rit.edu>
2019-11-09 16:27:16 -0500

    developed GetReplayGameRoute WIP

---
+++ src/main/java/com/webcheckers/ui/GetReplayGameRoute.java
@@ -0,0 +1,41 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.appl.GameLobby;
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Board;
+import com.webcheckers.model.Player;
+import spark.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class GetReplayGameRoute {
+    static final String VIEW_NAME = "game.ftl";
+    static final String BOARD_VIEW = "board";
+    private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
+    private final TemplateEngine templateEngine;
+    private final PlayerLobby playerLobby;
+
+    public GetReplayGameRoute(final TemplateEngine templateEngine, PlayerLobby playerLobby){
+        this.templateEngine = Objects.requireNonNull(templateEngine, "templateEngine is required");
+        this.playerLobby = playerLobby;
+        //
+        LOG.config("GetReplayMode is initialized.");
+    }
+
+    public Object handle(Response response, Request request){
+        Session httpSession = request.session();
+        Map<String, Object> vm = new HashMap<>();
+        Player player = httpSession.attribute("currentUser");
+        GameLobby gameLobby = playerLobby.getGameLobby(player);
+        ArrayList<Board> gameMoves = gameLobby.getGameMoves();
+        BoardView boardView = new BoardView(player, gameMoves.get(0));
+        vm.put(BOARD_VIEW, boardView);
+        return templateEngine.render(new ModelAndView(vm , VIEW_NAME));
+
+
+    }
+}
