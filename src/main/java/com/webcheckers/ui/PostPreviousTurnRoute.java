4407d19bb4a397f5ca5094e80b10f47ca25c090e
spencer josi <saj4750@rit.edu>
2019-11-09 15:23:31 -0500

    added PostNextTurnRoute.java and PostPreviousTurnRoute.java foundations

---
+++ src/main/java/com/webcheckers/ui/PostPreviousTurnRoute.java
@@ -0,0 +1,12 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.appl.PlayerLobby;
+
+public class PostPreviousTurnRoute {
+
+    private PlayerLobby playerLobby;
+
+    public PostPreviousTurnRoute(PlayerLobby playerLobby){
+        this.playerLobby = playerLobby;
+    }
+}
