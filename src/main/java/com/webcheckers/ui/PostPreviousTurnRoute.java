c4e40100bd72a181fda8ac4fbfbe4e3128209ffb
spencer josi <saj4750@rit.edu>
2019-11-10 23:33:22 -0500

    updated ui controllers

--- src/main/java/com/webcheckers/ui/PostPreviousTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostPreviousTurnRoute.java
@@ -22,12 +22,11 @@ public class PostPreviousTurnRoute implements Route{
     }
 
     public Object handle(Request request, Response response){
-        Session httpSession = request.session();
-        Message message;
+        String gameId = request.queryParams("id");
+        int gameIdInt = Integer.parseInt(gameId);
         ReplayLobby replayLobby = playerLobby.getReplayLobby();
-        //TODO implement gameId so that the game currently being replayed can be retrieved within the game arrayList
-        Game game = replayLobby.getGames().get(0); //hard coded for now
-        ArrayList<BoardState> boardStates = game.getBoardStates();
+        Game game = replayLobby.getGame(gameIdInt);
+        Message message;
         if(game.getCurrentState() - 1 >= 0){
             game.changeState(-1);
             message = Message.info("true");
